6aa883cac3364da85b5d5fac676a49aa41d100e7
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2024-04-18 09:06:38 +0700

    tam thoi the da

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -8,10 +8,12 @@ import ProductCard from "../components/Route/ProductCard/ProductCard";
 import styles from "../styles/styles";
 
 const ProductsPage = () => {
+  
   const [searchParams] = useSearchParams();
   const categoryData = searchParams.get("category");
   const {allProducts,isLoading} = useSelector((state) => state.products);
   const [data, setData] = useState([]);
+  const [active, setActive] = useState(1);
 
   useEffect(() => {
     if (categoryData === null) {
@@ -32,21 +34,104 @@ const ProductsPage = () => {
       <Loader />
     ) : (
       <div>
-      <Header activeHeading={3} />
-      <br />
-      <br />
-      <div className={`${styles.section}`}>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+        <Header activeHeading={3} />
+        <br />
+        <div className="flex items-center justify-between ml-20">
+          <div className="w-full flex">
+            <div className="flex items-center" onClick={() => setActive(1)}>
+              <h5
+                className={`font-[600] text-[20px] ${
+                  active === 1 ? "text-red-500" : "text-[#333]"
+                } cursor-pointer pr-[20px]`}
+              >
+                Products
+              </h5>
+            </div>
+            <div className="flex items-center" onClick={() => setActive(2)}>
+              <h5
+                className={`font-[600] text-[20px] ${
+                  active === 2 ? "text-red-500" : "text-[#333]"
+                } cursor-pointer pr-[20px]`}
+              >
+                All suppliers
+              </h5>
+            </div>
+
+            <div className="flex items-center" onClick={() => setActive(3)}>
+              <h5
+                className={`font-[600] text-[20px] ${
+                  active === 3 ? "text-red-500" : "text-[#333]"
+                } cursor-pointer pr-[20px]`}
+              >
+                Verified manufacturers
+              </h5>
+            </div>
+          </div>
         </div>
-        {data && data.length === 0 ? (
-          <h1 className="text-center w-full pb-[100px] text-[20px]">
-            No products Found!
-          </h1>
-        ) : null}
+        <br />
+        
+        {active === 1 && (
+          <div className="flex">
+              <div className="w-1/3 ">
+                <div className="w-11/12 mx-auto bg-white rounded ">
+                  <div className="ml-3 pt-5 text-[25px] text-center md:text-start font-[600] font-Roboto pb-[20px]" >Filters</div>
+                  <div className="flex gap-2">
+                    
+                  </div>
+                </div>
+              </div>
+              <div className="mr-5 grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-4 xl:gap-[30px] mb-12">
+                {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+              </div>
+              {data && data.length === 0 ? (
+                <h1 className="text-center w-full pb-[100px] text-[20px]">
+                  No products Found!
+                </h1>) : null}
+          </div>
+        )}
+
+        {active === 2 && (
+          <div className="flex">
+              <div className="w-1/3">
+                <div className="w-11/12 mx-auto bg-white rounded ">
+                  <div className="ml-3 pt-5 text-[25px] text-center md:text-start font-[600] font-Roboto pb-[20px]" >Filters</div>
+                  <div className="flex gap-2">
+                    
+                  </div>
+                </div>
+              </div>
+              <div className="w-2/3 mr-5 grid grid-cols-1 mb-12 ">
+                {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+              </div>
+              {data && data.length === 0 ? (
+                <h1 className="text-center w-full pb-[100px] text-[20px]">
+                  No products Found!
+                </h1>) : null}
+          </div>
+        )}
+
+        {active === 3 && (
+          <div className="flex">
+              <div className="w-1/3">
+                <div className="w-11/12 mx-auto bg-white rounded ">
+                  <div className="ml-3 pt-5 text-[25px] text-center md:text-start font-[600] font-Roboto pb-[20px]" >Filters</div>
+                  <div className="flex gap-2">
+                    
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 mb-12">
+                {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+              </div>
+              {data && data.length === 0 ? (
+                <h1 className="text-center w-full pb-[100px] text-[20px]">
+                  No products Found!
+                </h1>) : null}
+          </div>
+        )}  
+        
+        <Footer />
       </div>
-      <Footer />
-    </div>
     )
   }
   </>
